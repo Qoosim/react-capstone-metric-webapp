d1b57a1d0bb13d66cbcdc48f0258f93580629d39
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-03-11 15:19:46 +0100

    Reconfigure my reducer and action creators

--- src/redux/covid19/covid19.js
+++ src/redux/covid19/covid19.js
@@ -1,11 +1,36 @@
 const GET_COVID = 'covid-19/GET_DATA';
+const SET_DATA = 'covid-metrics/covid19/SET_DATA';
+const SET_IS_FIRST_RENDER = 'metrics-webapp/covid19/SET_IS_FIRST_RENDER';
+const SET_TODAYS_DATE = 'metrics-webapp/covid19/SET_TODAYS_DATE';
 
 const basedUrl = 'https://api.covid19tracking.narrativa.com/api/';
 
-const initialState = [];
+const initialState = {
+  data: null,
+  isFirstRender: true,
+  todaysDate: null,
+  covidDataTotalMock: {
+    today_confirmed: 0,
+    today_deaths: 0,
+    today_recovered: 0,
+    today_new_confirmed: 0,
+    today_new_deaths: 0,
+    today_new_recovered: 0,
+  },
+};
 
-export const getData = (payload) => ({
-  type: GET_COVID,
+export const setData = (payload) => ({
+  type: SET_DATA,
+  payload,
+});
+
+export const setIsFirstRender = (payload) => ({
+  type: SET_IS_FIRST_RENDER,
+  payload,
+});
+
+export const setTodaysDate = (payload) => ({
+  type: SET_TODAYS_DATE,
   payload,
 });
 
@@ -22,8 +47,21 @@ export const fetchDataFromApi = async () => {
 
 const covidReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_COVID:
-      return [...action.payload];
+    case SET_DATA:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    case SET_IS_FIRST_RENDER:
+      return {
+        ...state,
+        isFirstRender: action.payload,
+      };
+    case SET_TODAYS_DATE:
+      return {
+        ...state,
+        todaysDate: action.payload,
+      };
     default:
       return state;
   }
